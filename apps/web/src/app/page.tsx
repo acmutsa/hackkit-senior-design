82101b6091521313c3186209c98835473aa91e0a
xwilson03 <xanderwilson2014@gmail.com>
2023-10-28 02:21:29 -0500

    adds link to /dash from landing page logo

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,11 @@
+import Link from 'next/link';
 import Image from "next/image";
 import c from "@/hackkit.config";
 
 export default function Home() {
 	return (
 		<main className="min-h-screen flex flex-col items-center justify-center">
-			<Image src={c.icon.lg} alt="The HackKit Logo" width={400} height={400} />
+			<Link href="/dash"> <Image src={c.icon.lg} alt="The HackKit Logo" width={400} height={400} /> </Link>
 			<h1 className="font-sans font-black text-5xl mt-5">HackKit</h1>
 		</main>
 	);
