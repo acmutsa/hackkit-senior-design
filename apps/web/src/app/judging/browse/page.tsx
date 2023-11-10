595d3de62029a90439d1072c2ed0545291ce9a8e
xwilson03 <xanderwilson2014@gmail.com>
2023-11-09 21:42:35 -0600

    fixes submission data display

--- apps/web/src/app/judging/browse/page.tsx
+++ apps/web/src/app/judging/browse/page.tsx
@@ -8,8 +8,8 @@ export default async function Page() {
 
     interface Project {
       id:             string,
-      teamID:         string,
-      tableNum:       number,
+      team:           string,
+      table:          number,
       name:           string,
       track:          string,
       link:           string,
@@ -17,66 +17,42 @@ export default async function Page() {
     }
 
     const allSubmissions = await db.select().from(submissions);
-    const teamTags = await db.select({id: teams.id, tag: teams.tag}).from(teams);
+    const teamNames = await db.select({id: teams.id, name: teams.tag}).from(teams);
 
     const projects: Project[] = allSubmissions.map( (submission) => { return {
         id:             submission.id,
-        teamID:         teamTags.find((team) => {team.id == submission.teamID})?.tag || "undefined",
-        tableNum:       submission.table || 0,
+        team:           teamNames.find((team) => team.id == submission.teamID)?.name || "",
+        table:          submission.table || 0,
         name:           submission.name,
         track:          submission.track,
         link:           submission.link,
         submissionTime: submission.time.toDateString(),
     }});
 
-    console.log(projects);
-
-    // TODO: test data! remove later
-    // const projects: Project[] = [
-    //     {
-    //       id: 1, teamNum: 11, tableNum: 21,
-    //       name: "r/Slang", track: "Beginner",
-    //       link: "https://devpost.com/software/r-slang",
-    //       submissionTime: "2024-03-23T21:11:02.000Z"
-    //     },
-    //     {
-    //       id: 2, teamNum: 12, tableNum: 22,
-    //       name: "Optimealmmmmmmmmmmmmmmmmmmm", track: "General",
-    //       link: "https://devpost.com/software/optimeal-w8r1x3",
-    //       submissionTime: "2024-03-22T21:23:02.000Z"
-    //     },
-    //     {
-    //       id: 3, teamNum: 13, tableNum: 23, 
-    //       name: "RightBite", track: "Design",
-    //       link: "https://devpost.com/software/rightbite",
-    //       submissionTime: "2024-03-24T21:01:02.000Z"
-    //     },
-    // ]
-
 	return (
 		<div className="w-full mx-auto h-16">
-		  <div className="grid gap-2 p-2" style={{gridTemplateColumns: "repeat(auto-fit, minmax(410px,1fr)"}}>
+		  <div className="grid gap-2 p-2" style={{gridTemplateColumns: "repeat(auto-fit, minmax(400px,1fr)"}}>
               {
                 projects.map( (project) => { return (
                   <Card key={project.id} className="hover:bg-zinc-900 transition-colors">
-                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-4">
-                      <CardTitle className="text-sm font-medium">Team {project.teamNum.toString()}</CardTitle>
-                      <div> Table {project.tableNum} </div>
+                    <CardHeader className="flex flex-col items-center space-y-0 pb-4">
+                      <div className="text-sm font-medium">{project.track}</div>
+                      <div className="flex flex-row w-full justify-between">
+                        <div>{project.team}</div>
+                        <div>Table {project.table}</div>
+                      </div>
                     </CardHeader>
                     <CardContent>
-                      <div className="grid grid-rows-1 justify-between gap-x-2" style={{gridTemplateColumns: "1fr 94px"}}>
-                        <div className="text-3xl align-middle w-auto overflow-hidden text-ellipsis">
-                          <Link href={`submissions/${project.id}`}>
-                            {project.name}
-                          </Link>
-                        </div>
-                        <div className="flex flex-row justify-between gap-x-2">
-                          <a href={project.link} target="_blank" className="relative z-10">
-                            <Button className="text-xl p-2 text-center">
-                              Devpost
-                            </Button>
-                          </a>
-                        </div>
+                      <div className="flex flex-row justify-between gap-x-2">
+                        <Link href={`submissions/${project.id}`}
+                              className="text-3xl align-middle w-full overflow-hidden text-ellipsis whitespace-nowrap">
+                          {project.name}
+                        </Link>
+                        <a href={project.link} target="_blank">
+                          <Button className="text-xl p-2 text-center">
+                            Devpost
+                          </Button>
+                        </a>
                       </div>
                     </CardContent>
                   </Card>
