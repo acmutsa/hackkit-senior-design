fe38296c1cf7a475b96b322932c9e60504d3e8d3
xwilson03 <xanderwilson2014@gmail.com>
2023-11-09 19:54:35 -0600

    adds db fetch to submission browse page

--- apps/web/src/app/judging/browse/page.tsx
+++ apps/web/src/app/judging/browse/page.tsx
@@ -1,12 +1,14 @@
 import { Card, CardHeader, CardContent, CardTitle } from "@/components/shadcn/ui/card";
 import { Button } from "@/components/shadcn/ui/button";
 import Link from "next/link";
+import { db } from "@/db";
+import { teams, submissions } from "@/db/schema";
 
 export default async function Page() {
 
     interface Project {
-      id:             number,
-      teamNum:        number,
+      id:             string,
+      teamID:         string,
       tableNum:       number,
       name:           string,
       track:          string,
@@ -14,27 +16,42 @@ export default async function Page() {
       submissionTime: string,
     }
 
+    const allSubmissions = await db.select().from(submissions);
+    const teamTags = await db.select({id: teams.id, tag: teams.tag}).from(teams);
+
+    const projects: Project[] = allSubmissions.map( (submission) => { return {
+        id:             submission.id,
+        teamID:         teamTags.find((team) => {team.id == submission.teamID})?.tag || "undefined",
+        tableNum:       submission.table || 0,
+        name:           submission.name,
+        track:          submission.track,
+        link:           submission.link,
+        submissionTime: submission.time.toDateString(),
+    }});
+
+    console.log(projects);
+
     // TODO: test data! remove later
-    const projects: Project[] = [
-        {
-          id: 1, teamNum: 11, tableNum: 21,
-          name: "r/Slang", track: "Beginner",
-          link: "https://devpost.com/software/r-slang",
-          submissionTime: "2024-03-23T21:11:02.000Z"
-        },
-        {
-          id: 2, teamNum: 12, tableNum: 22,
-          name: "Optimealmmmmmmmmmmmmmmmmmmm", track: "General",
-          link: "https://devpost.com/software/optimeal-w8r1x3",
-          submissionTime: "2024-03-22T21:23:02.000Z"
-        },
-        {
-          id: 3, teamNum: 13, tableNum: 23, 
-          name: "RightBite", track: "Design",
-          link: "https://devpost.com/software/rightbite",
-          submissionTime: "2024-03-24T21:01:02.000Z"
-        },
-    ]
+    // const projects: Project[] = [
+    //     {
+    //       id: 1, teamNum: 11, tableNum: 21,
+    //       name: "r/Slang", track: "Beginner",
+    //       link: "https://devpost.com/software/r-slang",
+    //       submissionTime: "2024-03-23T21:11:02.000Z"
+    //     },
+    //     {
+    //       id: 2, teamNum: 12, tableNum: 22,
+    //       name: "Optimealmmmmmmmmmmmmmmmmmmm", track: "General",
+    //       link: "https://devpost.com/software/optimeal-w8r1x3",
+    //       submissionTime: "2024-03-22T21:23:02.000Z"
+    //     },
+    //     {
+    //       id: 3, teamNum: 13, tableNum: 23, 
+    //       name: "RightBite", track: "Design",
+    //       link: "https://devpost.com/software/rightbite",
+    //       submissionTime: "2024-03-24T21:01:02.000Z"
+    //     },
+    // ]
 
 	return (
 		<div className="w-full mx-auto h-16">
