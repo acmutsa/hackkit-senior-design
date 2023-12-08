d2b9809249bda245ac827101298658359ded060c
xwilson03 <xanderwilson2014@gmail.com>
2023-12-07 23:15:08 -0600

    implements schedule generation

--- apps/web/src/app/api/admin/schedule/route.ts
+++ apps/web/src/app/api/admin/schedule/route.ts
@@ -18,42 +18,95 @@ export async function POST(req: Request) {
 	// 	return new Response("Unauthorized", { status: 401 });
 	// }
 
-    // delete any previously generated schedules
+    // clear any previously generated schedules
     await db.delete(interviews);
 
-    // Fetch all track & submission data and judges
-    const trackSizes     = (await db.select({name: tracks.name, size: sql`count(${trackSubmissions.trackID})`}).from(tracks)
-                                    .innerJoin(trackSubmissions, eq(tracks.id, trackSubmissions.trackID))
-                                    .groupBy(tracks.id)
-                                    .orderBy(sql`count(${trackSubmissions.trackID}) desc`));
+    // Fetch all submissions, track data, and judges
+    const allSubmissions = (await db.select({id: submissions.id}).from(submissions).orderBy(submissions.id))
+                                .map((submission) => ({id: submission.id}));
 
-    const allSubmissions = (await db.select().from(submissions)
-                                    .innerJoin(trackSubmissions, eq(submissions.id, trackSubmissions.submissionID))
-                                    .innerJoin(tracks,           eq(trackSubmissions.trackID, tracks.id))
-                           ).map((obj) => (obj.submissions.id));
+    const trackSizes = (await db.select({id: tracks.id, name: tracks.name, size: sql`count(${trackSubmissions.trackID})`})
+                                .from(tracks).innerJoin(trackSubmissions, eq(tracks.id, trackSubmissions.trackID))
+                                .groupBy(tracks.id)
+                                .orderBy(sql`count(${trackSubmissions.trackID}) desc`));
 
-    const allJudges      = (await db.select().from(users).where(eq(users.role, "judge"))
-                           ).map((user) => ({
-                                id: user.clerkID
-                           }));
+    const allTrackSubmissions = (await db.select().from(trackSubmissions))
+                                    .map((trackSubmission) => ({
+                                        id: trackSubmission.id,
+                                        trackID: trackSubmission.trackID,
+                                        submissionID: trackSubmission.submissionID
+                                    }));
 
-    console.log(JSON.stringify(trackSizes));
-    console.log();
-    console.log(JSON.stringify(allSubmissions));
-    console.log();
-    console.log(JSON.stringify(allJudges));
+    const allJudges = (await db.select({id: users.clerkID}).from(users).where(eq(users.role, "judge")))
+                           .map((user) => ({id: user.id}));
 
-    // c.rooms;
+    // Declare interview array to store iteratively generated schedule data
+    type Interview = {
+        id:           number,
+        judgeID:      string,
+        submissionID: string,
+        table:        number,
+        complete:     boolean
+    };
 
-    // split submissions by track
+    var tempInterviews: Record<string, Interview[]> = {};
 
-    //foreach(track): make Map<String,String[]> entry of <track_name,null>
-    //foreach(project): map[project.track].append(project.id)
+    // Generate interviews in batches ordered by submission
+    var curID = 0;
+    for (let submission of allSubmissions) {
+        tempInterviews[submission.id] = [];
+        for (let i = 0; i < c.interviewsPerSubmission; i++) {
+            let newInterview = {
+                id: curID++,
+                judgeID: "",
+                submissionID: submission.id,
+                table: -1,
+                complete: false
+            };
+            tempInterviews[submission.id].push(newInterview);
+        }
+    }
 
+    // Assign tables to largest tracks' submissions first
+    var curTable = 1;
+    for (let track of trackSizes) {
+        for (let trackSubmission of allTrackSubmissions) {
 
-    // fit largest tracks into largest rooms
-    // ...
-    
+            if (trackSubmission.trackID != track.id) continue;
+
+            let newTableAssigned = false;
+            for (let curInterview of tempInterviews[trackSubmission.submissionID]){
+                if (curInterview["table"] == -1)
+                    curInterview["table"] = curTable;
+                    newTableAssigned = true;
+            }
+
+            if (newTableAssigned) curTable++;
+        }
+    }
+
+    // Unroll interviews record into array
+    var interviewsArray: Interview[] = [];
+    for (let key in tempInterviews) {
+        for (let interview of tempInterviews[key]) {
+            interviewsArray.push(interview);
+        }
+    }
+
+    const numInterviews = interviewsArray.length;
+    const numJudges = allJudges.length;
+    const interviewsPerJudge = Math.ceil(numInterviews / numJudges);
+
+    for (let judge = 0; judge < numJudges; judge++) {
+        for (let interview = 0; interview < interviewsPerJudge; interview++) {
+            const submission = judge * interviewsPerJudge + interview;
+            if (submission < numInterviews)
+                interviewsArray[submission]["judgeID"] = allJudges[judge].id;
+        }
+    }
+
+    await db.insert(interviews).values(interviewsArray);
+
     return NextResponse.json({success: true});
 }
 
